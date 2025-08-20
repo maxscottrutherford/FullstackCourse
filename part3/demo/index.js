2c607624ee80de44cfc1314c14370a6f667986b8
Max Rutherford <maxscottrutherford@gmail.com>
2025-08-19 22:03:14 -0700

    install express for exercises

--- part3/demo/index.js
+++ part3/demo/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [ //hardcoded list of notes in backend
     {
         id: "1",
@@ -25,7 +27,9 @@ app.get('/', (request, response) => {
 
 app.get('/api/notes', (request, response) => {
     response.json(notes)
+    console.log(new Date().toUTCString())
 })
+    
 
 app.get('/api/notes/:id', (request, response) => {
     const id = request.params.id
@@ -38,8 +42,35 @@ app.get('/api/notes/:id', (request, response) => {
     }
 })
 
+const generateId = () => {
+    maxId = notes.length > 0
+        ? Math.max(...notes.map(n => Number(n.id)))
+        : 0
+    return String(maxId + 1)
+}
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        id: generateId(),
+        content: body.content,
+        important: body.important || false
+    }
+
+    notes = notes.concat(note)
+
+    response.json(note)
+})
+
 app.delete('/api/notes/:id', (request, response) => {
-    const id = request.params.find
+    const id = request.params.id
     notes = notes.filter(note => note.id !== id)
 
     response.status(204).end()
