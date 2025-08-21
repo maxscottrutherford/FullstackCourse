717b626e371c458ccc624f7aa79cb25e8c8407ea
Max Rutherford <maxscottrutherford@gmail.com>
2025-08-20 17:36:53 -0700

    add morgan to demo in part 3

--- part3/demo/index.js
+++ part3/demo/index.js
@@ -1,7 +1,19 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path: ', request.path)
+    console.log('Body: ', request.body)
+    console.log('---')
+    next()
+}
+
 app.use(express.json())
+app.use(requestLogger)
+
 
 let notes = [ //hardcoded list of notes in backend
     {
@@ -27,7 +39,6 @@ app.get('/', (request, response) => {
 
 app.get('/api/notes', (request, response) => {
     response.json(notes)
-    console.log(new Date().toUTCString())
 })
     
 
@@ -75,6 +86,12 @@ app.delete('/api/notes/:id', (request, response) => {
 
     response.status(204).end()
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(400).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
---
+++ part3/phonebook_backend/index.js
@@ -0,0 +1,118 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    {
+        "id": "1",
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": "2",
+        "name": "Ada Lovelace",
+        "number": "39-44-5323532"
+    },
+    {
+        "id": "3",
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": "4",
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    },
+    {
+        "id": "5",
+        "name": "Test Guy",
+        "number": "123"
+    }
+]
+
+
+
+app.get('/', (request, response) => {
+    response.send('<h1>Phonebook</h1>')
+}) 
+
+app.get('/info', (request, response) => {
+    const reqTime = new Date().toString()
+    response.send(
+        `
+        <p>Phonebook has info for ${persons.length} people</p
+        <p> ${reqTime} </p>
+        `
+    )
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    }
+    else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+})
+
+const generateId = () => {
+    const minCeil = Math.ceil(4)
+    const maxFloor = Math.floor(1000)
+    return String(Math.floor(Math.random() * (maxFloor - minCeil) + minCeil))
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name && !body.number) {
+        return response.status(400).json({
+            error: 'empty entry'
+        })
+    }
+
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+
+    if (!body.number) {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+
+    if (persons.find(person => person.name.toLowerCase() === body.name.toLowerCase())) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
