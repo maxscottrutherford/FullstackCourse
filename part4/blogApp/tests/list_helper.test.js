7ac09f26372ca9c807e195dd71bf52b4943e07ee
Max Rutherford <maxscottrutherford@gmail.com>
2025-08-31 21:34:12 -0700

    add mostBlogs helper function and tests

--- part4/blogApp/tests/list_helper.test.js
+++ part4/blogApp/tests/list_helper.test.js
@@ -114,3 +114,64 @@ describe('favorite blog', () => {
   })
 
 })
+
+describe('most blogs', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const listWithMultipleBlogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    }
+  ]
+
+  test('of list with empty blogs returns null', () => {
+    const result = listHelper.mostBlogs([])
+    assert.deepStrictEqual(result, null)
+  })
+
+  test('of list with one blog returns that author and one blog', () => {
+    const result = listHelper.mostBlogs(listWithOneBlog)
+    assert.deepStrictEqual(result, {
+      author: 'Edsger W. Dijkstra',
+      blogs: 1
+    })
+  })
+
+  test('of list with multiple blogs return corrects values', () => {
+    const result = listHelper.mostBlogs(listWithMultipleBlogs)
+    assert.deepStrictEqual(result, {
+      author: 'Edsger W. Dijkstra',
+      blogs: 2
+    })
+  })
+})
--- part4/blogApp/utils/list_helper.js
+++ part4/blogApp/utils/list_helper.js
@@ -1,22 +1,37 @@
+const lodash = require('lodash')
+
 const dummy = (blogs) => {
   console.log('received blogs', blogs)
   return 1
 }
 
 const totalLikes = (blogs) => {
-  return blogs.reduce(function (acc, obj) { return acc + obj.likes; }, 0)
+  return blogs.reduce(function (acc, obj) { return acc + obj.likes }, 0)
 }
 
 const favoriteBlog = (blogs) => {
-  const favBlog = blogs.reduce((max, obj) => {
-    obj.likes > max.likes ? obj : max
-  })
+  return blogs.reduce((max, obj) => { return obj.likes > max.likes ? obj : max })
+}
 
-  return favBlog
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return null
+
+  // count blogs by author
+  const counts = lodash.countBy(blogs, 'author')
+
+  //convert counts to array of { author, blogs } objects
+  const authors = lodash.map(counts, (count, author) => ({
+    author,
+    blogs: count
+  }))
+
+  //return object with most blogs
+  return lodash.maxBy(authors, 'blogs')
 }
 
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
